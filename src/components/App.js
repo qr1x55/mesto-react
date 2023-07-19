f9985c75dd3b2737d7c03e5aabb934bea6555cea
Roman Bolotov <karrawhy@yandex.ru>
2023-07-19 08:32:45 +0300

    feat: registration and authorization, popups about login status

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,42 @@ import api from "../utils/api";
 import '../index.css';
 import { useState, useCallback, useEffect } from 'react';
 import CurrentUserContext from '../contexts/CurrentUserContext.js';
+import SendingContext from '../contexts/SendingContext.js';
 import EditProfilePopup from './EditProfilePopup.js';
 import EditAvatarPopup from './EditAvatarPopup.js';
 import AddPlacePopup from './AddPlacePopup.js';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute.js';
+import ProtectedHomepage from './ProtectedHomepage';
+import { registration, authorization, getUserData } from '../utils/auth.js';
+import InfoToolTip from './InfoTooltip.js';
+
 
 function App() {
   const [isEditPopupOpen, setEditPopupOpen] = useState(false)
   const [isAddPopupOpen, setAddPopupOpen] = useState(false)
   const [isAvatarPopupOpen, setAvatarPopupOpen] = useState(false)
   const [isDeletePopupOpen, setDeletePopupOpen] = useState(false)
+  const [isLoginPopupOpen, setLoginPopupOpen] = useState(false)
   const [selectedCard, setSelectedCard] = useState({})
   const [isImagePopup, setImagePopupOpen] = useState(false)
   const [isSending, setIsSending] = useState(false)
 
   const [currentUser, setCurrentUser] = useState({})
+  const [userEmail, setUserEmail] = useState('')
 
   const [cards, setCards] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [deleteCardId, setDeleteCardId] = useState('')
 
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [isCheckToken, setIsCheckToken] = useState(true)
+
   const isOpen = isEditPopupOpen || isAddPopupOpen || isAvatarPopupOpen || isDeletePopupOpen || isImagePopup
+
+  const navigate = useNavigate(); 
 
   const statesForCloseAllPopups = useCallback (() => {
     setEditPopupOpen(false);
@@ -34,11 +50,13 @@ function App() {
     setAvatarPopupOpen(false);
     setDeletePopupOpen(false);
     setImagePopupOpen(false);
+    setLoginPopupOpen(false);
   },[])
 
   const closeAllPopups = useCallback (() => {
     statesForCloseAllPopups()
   },[statesForCloseAllPopups])
+
 
   useEffect(() => {
     function closeByEsc(e) {
@@ -60,6 +78,22 @@ function App() {
       }
     }
   }, [isOpen, closeAllPopups])
+
+  useEffect(() => {
+    if (localStorage.jwt) {
+      getUserData(localStorage.jwt)
+      .then(res => {
+        setUserEmail(res.data.email)
+        setLoggedIn(true)
+        setIsCheckToken(false)
+        navigate('/')
+      })
+      .catch((error => console.error(`Ошибка при загрузке ${error}`)))
+    } else {
+      setLoggedIn(false)
+      setIsCheckToken(false)
+    }
+  }, [navigate])
 
   function handleEditClick () {
     setEditPopupOpen(true);
@@ -99,16 +133,17 @@ function App() {
   }
 
   useEffect(() => {
-    setIsLoading(true)
-    Promise.all([api.getInfo(), api.getInitialCards()])
-    .then(([userData, cardsData]) => {
-      setCurrentUser(userData);
-      cardsData.forEach(item => item.userId = userData._id);
-      setCards(cardsData)
-      setIsLoading(false)
-    })
-    .catch((error => console.error(`Ошибка при загрузке ${error}`)))
-  }, [])
+    if (loggedIn) {
+      setIsLoading(true)
+      Promise.all([api.getInfo(), api.getInitialCards()])
+      .then(([userData, cardsData]) => {
+        setCurrentUser(userData);
+        cardsData.forEach(item => item.userId = userData._id);
+        setCards(cardsData)
+        setIsLoading(false)
+      })
+      .catch((error => console.error(`Ошибка при загрузке ${error}`)))}
+  }, [loggedIn])
 
   function handleUpdateUser(userData, reset) {
     setIsSending(true)
@@ -149,37 +184,93 @@ function App() {
       .finally(() => setIsSending(false))
   }
 
+  function handleSignIn(password, email) {
+    setIsSending(true)
+    authorization(password, email)
+      .then(res => {
+        localStorage.setItem('jwt', res.token)
+        setLoggedIn(true)
+        window.scrollTo(0,0)
+        navigate('/')
+      })
+      .catch((error) => {
+        setLoginPopupOpen(true)
+        setIsSuccess(false)
+        console.error(`Ошибка при регистрации ${error}`);
+      })
+      .finally(() => setIsSending(false))
+  }
+
+  function handleSignUp(password, email) {
+    setIsSending(true)
+    registration(password, email)
+      .then(() => {
+        setLoginPopupOpen(true)
+        setIsSuccess(true)
+        window.scrollTo(0,0)
+        navigate('/sign-in')
+      })
+      .catch((error) => {
+        setLoginPopupOpen(true)
+        setIsSuccess(false)
+        console.error(`Ошибка при регистрации ${error}`);
+      })
+      .finally(() => setIsSending(false))
+  }
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
-      <Header />
-      <Main 
-        onEdit={handleEditClick}
-        onAdd={handleAddClick}
-        onAvatar={handleAvatarClick}
-        onDelete={handleDeleteClick}
-        onImageClick={handleImageClick}
-        cards={cards}
-        isLoading={isLoading}
-      />
+      <SendingContext.Provider value={isSending}>
+        <Routes>
+          <Route path='/' element={<ProtectedRoute
+          element={ProtectedHomepage}
+          onEdit={handleEditClick}
+          onAdd={handleAddClick}
+          onAvatar={handleAvatarClick}
+          onDelete={handleDeleteClick}
+          onImageClick={handleImageClick}
+          cards={cards}
+          isLoading={isLoading}
+          userEmail={userEmail}
+          loggedIn={loggedIn}
+          isCheckToken={isCheckToken}
+          />}/>
+          <Route path='/sign-up' element={
+            <>
+              <Header name='signup'/>
+              <Main name='signup' isCheckToken={isCheckToken} handleSignUp={handleSignUp}/>
+            </>
+          }/>
+          <Route path='/sign-in' element={
+            <>
+              <Header name='signin'/>
+              <Main name='signin' isCheckToken={isCheckToken} handleSignIn={handleSignIn}/>
+            </>
+          }/>
+            
+          <Route path='*' element={<Navigate to='/' replace/>}/>
+        </Routes>
+      </SendingContext.Provider>
       <Footer />
       <EditProfilePopup         
         onAddPlace = {handleUpdateUser}
         isOpen = {isEditPopupOpen} 
         onClose= {closeAllPopups}
         isSending = {isSending}
-      /> 
+        onUpdateUser={handleUpdateUser}
+      />
       <AddPlacePopup
         onAddPlace = {handleAddPlace}
         isOpen = {isAddPopupOpen} 
         onClose= {closeAllPopups}
         isSending = {isSending}
-      />
+      /> 
       <EditAvatarPopup
         onUpdateAvatar = {handleUpdateAvatar}
         isOpen = {isAvatarPopupOpen} 
         onClose= {closeAllPopups}
         isSending = {isSending}
-      />
+      /> 
       <PopupWithForm 
         name='delete' 
         title='Вы уверены?' 
@@ -188,11 +279,18 @@ function App() {
         onClose = {closeAllPopups}
         onSubmit = {handleDeleteCard}
         isSending = {isSending}
-      />
+      /> 
       <ImagePopup 
+        name = 'image'
         card = {selectedCard} 
         isOpen = {isImagePopup} 
         onClose = {closeAllPopups}
+      /> 
+      <InfoToolTip
+        name='login'
+        isSuccess={isSuccess}
+        isOpen={isLoginPopupOpen}
+        onClose={closeAllPopups}
       />
     </CurrentUserContext.Provider>
   );
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,16 +1,20 @@
-function ImagePopup({card, isOpen, onClose}) {
+import Popup from "./Popup.js";
+
+
+function ImagePopup({name, card, isOpen, onClose}) {
   return (
-    <div className={`popup popup_type_picture ${isOpen ? 'popup_opened' : ''}`} onClick={onClose}>
-      <div className="popup__container popup__container_type_picture" onClick={e => e.stopPropagation()}>
-        <img 
-          className="popup__picture" 
-          src={card.link} 
-          alt={`Иллюстрация ${card.name}`} 
-        />
-        <h2 className="popup__caption">{card.name}</h2>
-        <button type="button" className="popup__exit-button button" onClick={onClose}></button>
-      </div>
-    </div>
+    <Popup 
+    name={name}
+    isOpen={isOpen}
+    onClose={onClose}
+    >
+      <img 
+        className="popup__picture" 
+        src={card.link} 
+        alt={`Иллюстрация ${card.name}`} 
+      />
+      <h2 className="popup__caption">{card.name}</h2>
+    </Popup>
   )
 }
 
